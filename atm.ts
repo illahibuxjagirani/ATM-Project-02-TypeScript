37cf3bc7baa2e779e24a7e6b322355cd00684e03
ILLAHIB <155870604+ILLAHIB@users.noreply.github.com>
2024-01-07 11:02:24 +0500

    Add files via upload

---
+++ atm.ts
@@ -0,0 +1,70 @@
+
+import inquirer from "inquirer";
+
+interface UserInput {
+    userID: string,
+    userPin: number,
+    accountType: string,
+    transactionType: string,
+    amount: number
+}
+
+const userInput: UserInput = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userID",
+        message: "Enter User ID"
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Enter your Pin"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving"],
+        message: "Select Your Account Type"
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Cash Withdraw", "Balance Inquiry"],
+        message: "Select your transaction"
+    },
+    {
+       type: "number",
+       name: "amount",
+       message: "Enter amount you want to withdraw",
+       when(userInput){
+        return userInput.transactionType === "Cash Withdraw"
+       }
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 5000, 10000, 20000, 25000],
+        message: "Select amount you want to withdraw",
+        when(userInput){
+            return userInput.transactionType === "Fast Cash"
+        }
+    }
+]);
+
+// making variables of user input data
+
+const userID = userInput.userID;
+const userPin = userInput.userPin;
+const enteredAmount = userInput.amount;
+
+if((userID && userPin) && userInput.transactionType === "Balance Inquiry"){
+    const userBalance = Math.floor(Math.random() * 100000);
+    console.log(`Your current balance is Rs ${userBalance}\n`)
+}else if(userID && userPin){
+    const userBalance2 = Math.floor(Math.random() * 100000);
+    if(userBalance2 > enteredAmount){
+        console.log(`Your account has been debited with Rs ${enteredAmount} and your remaining balance is ${userBalance2 - enteredAmount}`);
+    }else{
+        console.log(`\n Unsufficient Balance`);
+    }
+}
